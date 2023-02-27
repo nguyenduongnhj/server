1ea07d7f83875b5e148279940839445fba8485eb
doragon <op290420@gmail.com>
2023-02-27 10:15:41 +0700

    update scrpit

--- start.sh
+++ start.sh
@@ -69,32 +69,32 @@ rm -rf $FOLDER_APPS_PATH/spreed $FOLDER_APPS_PATH/tasks $FOLDER_APPS_PATH/calend
 nvm use 15.14.0
 
 #wait
-git clone https://github.com/kma-custom-nextcloud/tasks.git $FOLDER_APPS_PATH/tasks
+git clone https://github.com/nguyenduongnhj/tasks.git $FOLDER_APPS_PATH/tasks
 cd $FOLDER_APPS_PATH/tasks
 make
 
 nvm use 14.14.0
 
 #wait
-git clone https://github.com/kma-custom-nextcloud/spreed.git $FOLDER_APPS_PATH/spreed
+git clone https://github.com/nguyenduongnhj/spreed.git $FOLDER_APPS_PATH/spreed
 cd $FOLDER_APPS_PATH/spreed
 make dev-setup
 make build-js
 
 #wait
-git clone https://github.com/kma-custom-nextcloud/calendar.git $FOLDER_APPS_PATH/calendar
+git clone https://github.com/nguyenduongnhj/calendar.git $FOLDER_APPS_PATH/calendar
 cd $FOLDER_APPS_PATH/calendar
 make dev-setup
 make build-js
 
 #wait
-git clone https://github.com/kma-custom-nextcloud/deck.git $FOLDER_APPS_PATH/deck
+git clone https://github.com/nguyenduongnhj/deck.git $FOLDER_APPS_PATH/deck
 cd $FOLDER_APPS_PATH/deck
 make install-deps
 make build
 
 #wait
-git clone https://github.com/kma-custom-nextcloud/richdocuments.git $FOLDER_APPS_PATH/richdocuments
+git clone https://github.com/nguyenduongnhj/richdocuments.git $FOLDER_APPS_PATH/richdocuments
 cd $FOLDER_APPS_PATH/richdocuments
 npm ci
 npm run dev
